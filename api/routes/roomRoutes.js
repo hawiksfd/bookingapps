16eee19fb1a19f6c035a7aa05fc6b3d4550b76f7
hawiksfd <hawiksyaef@gmail.com>
2022-06-06 17:23:03 +0700

    Menambah model relation

--- api/routes/roomRoutes.js
+++ api/routes/roomRoutes.js
@@ -1,5 +1,24 @@
 import express from "express"
+import { createRoom, updateRoom, deleteRoom, getRoom, getRooms } from "../controllers/roomContr.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 
 const router = express.Router();
+
+
+//CREATE
+router.post("/:hotelId", verifyAdmin, createRoom)
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom)
+
+//DELETE
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom)
+
+//GET
+router.get("/:hotelId", getRoom)
+
+//GET ALL
+router.get("/", getRooms)
 
 export default router
